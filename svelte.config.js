bf721f8ab6a796a18d30e026c42872cd0a51a07c
stephaniedang <stephanietumidang@gmail.com>
2023-12-01 00:39:29 -0500

    pages updates

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+    kit: {
+        appDir: 'app', // Required as the default is _app
+        adapter: adapter()
+    },
+    preprocess: vitePreprocess()
 };
-
 export default config;
