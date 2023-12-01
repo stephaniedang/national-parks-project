d4e146395913b341f5e146f885f407078db16914
stephaniedang <stephanietumidang@gmail.com>
2023-12-01 00:48:11 -0500

    Revert "pages updates"
    This reverts commit bf721f8ab6a796a18d30e026c42872cd0a51a07c.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        appDir: 'app', // Required as the default is _app
-        adapter: adapter()
-    },
-    preprocess: vitePreprocess()
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
+
 export default config;
